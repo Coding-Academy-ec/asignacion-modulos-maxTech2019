22c9f5fef4ba2be6cf32e109e839f243da0cbd49
Fredy M. Jordan C <fmaxjordan@gmail.com>
2024-04-26 15:53:32 -0500

    version final

--- programa.py
+++ programa.py
@@ -4,6 +4,6 @@ a = 10
 b = 5
 
 print("Suma:", operaciones.suma(a, b)) # Resultado de la suma
-# Resultado de la resta
-# Resultado de la multiplicación
+print("Resta:", operaciones.resta(a, b)) # Resultado de la resta
+print("Multiplicación:", operaciones.multiplicacion(a, b))# Resultado de la multiplicación
 print("División:", operaciones.division(a, b)) # Resultado de la división
